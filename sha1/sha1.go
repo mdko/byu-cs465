bea461f7398ff4ec3f5784b6ffe56b613cf0db41
Michael Christensen <chmdko@gmail.com>
2013-09-20 04:02:53 -0600

    both attacks implemented, only run once

--- sha1/sha1.go
+++ sha1/sha1.go
@@ -4,46 +4,70 @@ import (
 	"crypto/sha1"
 	"math/rand"
 	"encoding/binary"
+	"time"
 	"fmt"
 )
 
+var debug bool = false
+
+// Input strings of length of n + 1, n = number of bits in hash
+var collisionNbits int = 30
+var preimageNbits int = 18
+
+var numTrials int = 100
+
+
+func debugPrint(message string) {
+	if (debug) {
+		fmt.Printf("DEBUG: %s\n", message)
+	}
+}
+
+func getBitMask(numHighBits int) (mask uint32) {
+	mask = 1
+	mask = (mask << uint16(numHighBits)) - 1
+	debugPrint(fmt.Sprintf("Bit mask for nbit %v: %b\n", numHighBits, mask))
+	return
+}
+
 func main() {
-	var nbits int = 20
-	// input
-	// 2^n unique input strings
-	// length of n + 1, n = number of bits in hash
-	// so if n = 20, 2^20 = 1,048,576 values of length 21 bits
+	runCollisionAttack(numTrials)
+	run2ndPreimageAttack(numTrials)
+}
 
-	// output
-	// truncate to the number of bits that lets me get attack in a few seconds
-
-	//------------------------------------------------------------------------
-	//------------------------------------------------------------------------
+func runCollisionAttack(numTrials int) {
+	//-------------------------------------------------------------------------------------------------------
+	//-------------------------------------------------------------------------------------------------------
 	// Collision attack:
 	// Find two separate input messages that both hash to same digest
 	// Expected Time: 2^(n/2)
-	//------------------------------------------------------------------------
-	generatedMessages := make(map[uint32]uint32, 2^nbits) // Hold 32 bit integers, but the last 11 bits will be zeroed
+	// Eg. for n = 20, 2^10 = 1024 ~ approximately number of tries needed
+	//-------------------------------------------------------------------------------------------------------
+	fmt.Println("Running Collision Attack")
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	generatedMessages := make(map[uint32]uint32, 2^collisionNbits) 	// Hold 32 bit integers, but the last 11 bits will be zeroed
 	var numTries int = 0
 	for {	
 		numTries++
 		
 		// Generate new message m1 that is n + 1 bits long
-		m1 := rand.Uint32() & 0x001FFFFF // nbits + 1 (21) bits long, bits 22-32 zeroed out
+		m1 := rand.Uint32() & getBitMask(collisionNbits + 1)			// nbits + 1 (21) bits long, bits 22-32 zeroed out
 		buf := make([]byte, 4)
-		fmt.Printf("Message m1: %x\n", m1)
+		debugPrint(fmt.Sprintf("Message m1: %x\n", m1))
 		
 		// Put it in a byte array for use in the hash function (cast to uint64 because that's what function accepts)
-		binary.PutUvarint(buf, uint64(m1))
+		binary.LittleEndian.PutUint32(buf, m1)
+		debugPrint(fmt.Sprintf("Buf: %x\n", buf))
 		
 		// Generate hash
 		h := sha1.New()
 		h.Write(buf)
-		hashArr := h.Sum(nil) 						// a []byte
-		hash64, _ := binary.Uvarint(hashArr) 		// a uint64
-		hash := uint32(hash64) & 0x0000FFFFF		// nbits (20) long
-		fmt.Printf("Hash: %x\n\n", hash)
-		//h.Reset()
+		hashArr := h.Sum(nil) 											// a []byte
+		hash32 := binary.LittleEndian.Uint32(hashArr) 					// a uint32
+		hash := hash32 & getBitMask(collisionNbits)					// nbits (20) long
+		debugPrint(fmt.Sprintf("Hash: %x\n\n", hash))
+		//h.Reset()														// unneeded since I use a new hash object in each loop
 
 		// Check to see if we've already discovered this hash by searching the map
 		// If there is a match in the map, m2 = value associated with that hash in map
@@ -51,19 +75,75 @@ func main() {
 		m2, present := generatedMessages[hash]
 		if present {
 			if (m2 != m1) {
-				fmt.Println("Found two messages that hash to same digest but aren't the same")
-				fmt.Printf("%x\n%x\n", m1, m2)
-				fmt.Printf("in %v tries\n", numTries)
+				debugPrint(fmt.Sprint("Found two messages that hash to same digest but aren't the same"))
+				debugPrint(fmt.Sprintf("%x\n%x\n", m1, m2))
 				break
 			}
 		} else {
 			generatedMessages[hash] = m1
 		}
 	}
-	//------------------------------------------------------------------------
-	//------------------------------------------------------------------------
-	// Preimage attack:
-	// Given a hash value, find the source message that hashes to that value
+	fmt.Printf("Collision Attack: For a message of %v bits and hash of %v bits, took %v tries\n", 
+		collisionNbits+1, collisionNbits, numTries)
+}
+
+func run2ndPreimageAttack(numTrials int) {
+	//-------------------------------------------------------------------------------------------------------
+	//-------------------------------------------------------------------------------------------------------
+	// 2nd Preimage attack:
+	// Given a message m1, find another message m2 s. that m1 != m2 and hash(m1) = hash(m2)
 	// Expected Time: 2^n
-	//------------------------------------------------------------------------
+	// Eg. for n = 18, 2^n = 262,144 tries
+	//-------------------------------------------------------------------------------------------------------
+	fmt.Println("Running 2nd Pre-image Attack")
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Generate new message m1 that is n + 1 bits long
+	m1 := rand.Uint32() & getBitMask(preimageNbits + 1)
+	buf := make([]byte, 4)
+	debugPrint(fmt.Sprintf("Message m1: %x\n", m1))
+	
+	// Put it in a byte array for use in the hash function (cast to uint64 because that's what function accepts)
+	binary.LittleEndian.PutUint32(buf, m1)
+	debugPrint(fmt.Sprintf("Buf: %x\n", buf))
+	
+	// Generate hash
+	h := sha1.New()
+	h.Write(buf)
+	hashArr := h.Sum(nil) 									// a []byte
+	hash32 := binary.LittleEndian.Uint32(hashArr) 			// a uint32
+	m1hash := hash32 & getBitMask(preimageNbits)			// nbits (20) long
+	h.Reset()
+	debugPrint(fmt.Sprintf("Hash: %x\n\n", m1hash))
+
+	var numTries int = 0
+	for {	
+		numTries++
+		
+		// Generate new message m2 that is n + 1 bits long
+		m2 := rand.Uint32() & getBitMask(preimageNbits + 1)
+		buf = make([]byte, 4)
+		debugPrint(fmt.Sprintf("Message m2: %x\n", m2))
+		
+		// Put it in a byte array for use in the hash function (cast to uint64 because that's what function accepts)
+		binary.LittleEndian.PutUint32(buf, m2)
+		debugPrint(fmt.Sprintf("Buf: %x\n", buf))
+		
+		// Generate hash
+		h = sha1.New()
+		h.Write(buf)
+		hashArr = h.Sum(nil) 								// a []byte
+		hash32 = binary.LittleEndian.Uint32(hashArr) 		// a uint32
+		m2hash := hash32 & getBitMask(preimageNbits)		// nbits (20) long
+		debugPrint(fmt.Sprintf("Hash: %x\n\n", m2hash))
+		//h.Reset()											// unneeded since I use a new hash object in each loop
+
+		if (m2hash == m1hash) && (m1 != m2) {
+			debugPrint(fmt.Sprint("Found two messages that hash to same digest but aren't the same"))
+			debugPrint(fmt.Sprintf("%x\n%x\n", m1, m2))
+			break
+		}
+	}
+	fmt.Printf("Pre-image attack: For a message of %v bits and hash of %v bits, took %v tries\n", 
+		preimageNbits+1, preimageNbits, numTries)
 }
